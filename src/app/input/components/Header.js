56dce2215aa588f6d13f15bcc98bf83cf75f4756
bigdaditor <bigdaditor1015@gmail.com>
2025-07-23 09:44:47 +0900

    feat: 안내문구 삭제

--- src/app/input/components/Header.js
+++ src/app/input/components/Header.js
@@ -12,7 +12,6 @@ export default function Header({ onGoToMain }) {
       </div>
       <div className={styles.headerCenter}>
         <h1>매출 입력</h1>
-        <p>반찬가게 매출을 입력하고 날씨 정보와 함께 저장하세요</p>
       </div>
       <div className={styles.headerRight}>
         {/* 우측 여백을 위한 빈 div */}
